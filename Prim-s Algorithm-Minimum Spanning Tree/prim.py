a44660ecf7a6b6e686cecf2ae6928e11512c0bc6
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-01-07 05:59:54 +0530

    Finalized almost all the aesthetic changes--only comments to be added now

--- Prim-s Algorithm-Minimum Spanning Tree/prim.py
+++ Prim-s Algorithm-Minimum Spanning Tree/prim.py
@@ -5,15 +5,15 @@ Created on Fri Jul  6 20:28:19 2018
 @author: Shashwat Kathuria
 """
 
-class Edge:
-    def __init__(self, start, end, weight):
-        self.start = start
-        self.end = end
+class UndirectedEdge:
+    def __init__(self, u, v, weight):
+        self.u = u
+        self.v = v
         self.weight = weight
     def __str__(self):
-        return "Start = {}  End = {}  Weight = {}".format(self.start, self.end, self.weight)
+        return "Between {} and {}  Weight = {}".format(self.u, self.v, self.weight)
 
-class Graph:
+class UndirectedGraph:
 
     def __init__(self, noOfVertices):
         self.edges  = []
@@ -27,26 +27,29 @@ class Graph:
 
 
 def main():
-
-    file = open("PRIM.txt", "r")
+    try:
+        file = open("PRIM.txt", "r")
+    except:
+        print("Could Not Open The Input File. Error.")
+        return
     graphInfo = file.readline().split(" ")
     noOfVertices = int(graphInfo[0])
     noOfEdges = int(graphInfo[1])
-    g = Graph(noOfVertices)
+    g = UndirectedGraph(noOfVertices)
 
     for i in range(noOfEdges):
         tempEdge = file.readline().split(" ")
-        e = Edge(start = int(tempEdge[0]), end = int(tempEdge[1]), weight = int(tempEdge[2]))
+        e = UndirectedEdge(u = int(tempEdge[0]), v = int(tempEdge[1]), weight = int(tempEdge[2]))
         g.addEdge(e)
         print("EDGE ADDED :  " + str(e))
 
-    print("Minimum Cost Spanning Tree Cost:" + str(PRIM(graph = g, startVertex = 1)))
+    print("\n" + "Minimum Cost Spanning Tree Cost:" + str( PRIM(graph = g, startVertex = 1) ) + "\n")
 
 
 
 
 def PRIM(graph,startVertex):
-    print(" COMPUTING MST .... ")
+    print("\n" + "COMPUTING MST .... " + "\n")
     cost = 0
     visited = [ startVertex ]
     notvisited = list(range(1, 500, 1))
@@ -57,13 +60,13 @@ def PRIM(graph,startVertex):
         cutsEdges = []
         flag = False
         for edge in edges:
-            if edge.start in visited and edge.end in notvisited:
+            if edge.u in visited and edge.v in notvisited:
                 flag = True
-                cutsEdges.append((edge.weight, edge.start, edge.end))
+                cutsEdges.append((edge.weight, edge.u, edge.v))
 
-            if edge.start in notvisited and edge.end in visited:
+            if edge.u in notvisited and edge.v in visited:
                 flag=True
-                cutsEdges.append((edge.weight, edge.end, edge.start))
+                cutsEdges.append((edge.weight, edge.v, edge.u))
 
         if flag==True :
             cutsEdges.sort(key = lambda fn : fn[0])
@@ -71,7 +74,7 @@ def PRIM(graph,startVertex):
             cost+=edgechosen[0]
             visited.append(edgechosen[2])
             notvisited.remove(edgechosen[2])
-            
+
     return cost
 
 
