50a7161963d82e8e7c3dfc7af9615c0a08aeb0b8
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-01-09 05:09:14 +0530

    Only comments to be added.Rest all is completed.

--- Travelling Salesman Problem - Hamiltonian Path - Dynamic Programming/TSP.py
+++ Travelling Salesman Problem - Hamiltonian Path - Dynamic Programming/TSP.py
@@ -4,119 +4,95 @@ Created on Mon Jul  9 11:06:39 2018
 
 @author: Shashwat Kathuria
 """
-import time
-start = time.time()
-file = open("tsp5.txt", "r")
-noOfCities = int(file.readline())
 
-cities = {}
-for i in range(noOfCities):
-    tempCityInfo = file.readline().split()
-    cityCoordinates = [ float(tempCityInfo[0]), float(tempCityInfo[1]) ]
-    cities[ i + 1 ] = cityCoordinates[:]
+import itertools
 
-cityIndicesExcluding1 = list( range(2, noOfCities + 1) )
+def main():
+
+    file = open("tsp_small.txt", "r")
+    noOfCities = int(file.readline())
+
+    cities = {}
+    for i in range(noOfCities):
+        tempCityInfo = file.readline().split()
+        cityCoordinates = [ float(tempCityInfo[0]), float(tempCityInfo[1]) ]
+        cities[ i + 1 ] = cityCoordinates[:]
+
+    cityIndicesExcluding1 = list( range(2, noOfCities + 1) )
+
+    citiesDistance = {}
+    for i in range(1, noOfCities + 1):
+        for j in range(1, noOfCities + 1):
+            citiesDistance[i, j] = distanceBetweenTwoCities(cities[i], cities[j])
+
+
+    TSP(noOfCities, cityIndicesExcluding1, citiesDistance)
+
 
 def distanceBetweenTwoCities(cityA, cityB):
 
     return ( ( (cityA[0] - cityB[0]) ** 2 ) + ( (cityA[1] - cityB[1]) ** 2) ) ** 0.5
 
 
-#noOfCities=4
-dist={}
-#dist[1,1]=0
-#dist[1,2]=2
-#dist[1,3]=1
-#dist[1,4]=3
-#dist[2,1]=2
-#dist[2,2]=0
-#dist[2,3]=4
-#dist[2,4]=9
-#dist[3,1]=1
-#dist[3,2]=4
-#dist[3,3]=0
-#dist[3,4]=6
-#dist[4,1]=3
-#dist[4,2]=9
-#dist[4,3]=6
-#dist[4,4]=0
-#cityIndicesExcluding1=[2,3,4]
-for i in range(1, noOfCities + 1):
-    for j in range(1, noOfCities + 1):
-       dist[i, j] = distanceBetweenTwoCities(cities[i], cities[j])
+def getSubsets(wholeSet, subsetSizeWithout1):
 
-
-def getSubsets(fullset, subsetSizeWithout1):
-  import itertools
   subsets = []
-  X = list( itertools.combinations(fullset, subsetSizeWithout1) )
+  X = list( itertools.combinations(wholeSet, subsetSizeWithout1) )
   for x in X:
       x = ( 1, ) + x
       subsets.append( x )
   return subsets
 
 def tupleCopyWithoutElement(s, element):
-
     tupleCopy = ()
-
     for iteratedElement in s:
         if iteratedElement == element:
             continue
         else:
-            tupleCopy += ( element, )
-
+            tupleCopy += ( iteratedElement, )
     return tupleCopy
 
 
-def TSP():
+def TSP(noOfCities, cityIndicesExcluding1, citiesDistance):
     a = {}
     a[ ( 1, ), 1 ] = 0
-    for subsetSizeWithout1 in range(1, noOfCities):#4
-        #subset of size m+1
+    for subsetSizeWithout1 in range(1, noOfCities):
+        # Subset is of size subsetSizeWithout1 + 1
+        print("COMPUTING. Finishes at " + str(noOfCities - 1) + " :" + str(subsetSizeWithout1))
         if subsetSizeWithout1 > 3:
             b = {}
-            for x in a:
-                if len(x[0]) == subsetSizeWithout1:#-1 or len(x[0])==m:
-                    b[x] = a[x]
-            a=b
-        print(m)
+            for key in a:
+                if len(key[0]) == subsetSizeWithout1:
+                    b[key] = a[key]
+            a = b
         S = getSubsets(cityIndicesExcluding1, subsetSizeWithout1)
-    #    print(S)
         for s in S:
             a[s, 1] = 99999999
 
         for s in S:
-    #        print(s)
-            possibilities=[]
+            possibilities = []
             for j in s[1:]:
-                possibilities=[]
+                possibilities = []
                 for k in s:
-    #                print("j",j)
-    #                print("k",k)
-                    flag = False
-                #    if (m+1)>2 and k==1:
-                 #       flag=True
-                    if k != j:# and flag==False:
-                        p = tupleCopyWithoutElement(s,j)
-    #                    print(a[p,k]+dist[k,j])
-                        possibilities.append( a[p, k] + dist[k, j] )
-    #            print(possibilities)
+                    if k != j:
+                        tupleCopy = tupleCopyWithoutElement(s,j)
+                        possibilities.append( a[tupleCopy, k] + citiesDistance[k, j] )
                 try:
                     minpath = min(possibilities)
                     a[s, j] = minpath
-     #            print(minpath)
                 except:
                     continue
 
-    realans = []
-    almostcompletepath = ()
+    realAnswer = []
+    almostCompletePath = ()
     for x in range(1, noOfCities + 1):
-        almostcompletepath += ( x,)
+        almostCompletePath += ( x,)
     for j in cityIndicesExcluding1:
-        print(j, a[almostcompletepath, j])
-        realans.append( a[almostcompletepath, j] + dist[j, 1] )
+        # print(j, a[almostCompletePath, j])
+        realAnswer.append( a[almostCompletePath, j] + citiesDistance[j, 1] )
 
-    print( min(realans) )
-end = time.time()
-x2 = float(end - start)
-print(end - start)
+    print("The optimal(minimum) length Hamiltonian path distance is : " +  str( min(realAnswer) ) )
+
+
+if __name__ == "__main__":
+    main()
