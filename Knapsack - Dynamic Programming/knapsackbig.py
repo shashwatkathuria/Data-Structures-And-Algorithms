86fb9a8395fd16a8dc98bcd451529e4830c3b158
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-01-08 19:06:35 +0530

    All necessary changes to knapsack function body and main function made...Only comments to be added.

--- Knapsack - Dynamic Programming/knapsackbig.py
+++ Knapsack - Dynamic Programming/knapsackbig.py
@@ -2,41 +2,43 @@
 """
 Created on Sun Jul  8 15:17:15 2018
 
-@author: shash
+@author: Shashwat Kathuria
 """
-
-file=open("BigKnapsack.txt","r")
-s=file.readline()
-t=s.split(" ")
-t=[int(t[0]),int(t[1])]
-noofitems=t[1]
-capacity=t[0]
-weights=[]
-values=[]
-for i in range(noofitems):
-    t=file.readline().split(" ")
-    weights.append(int(t[1]))
-    values.append(int(t[0]))
-ans={}
-
-def knapsack(ans,size,noofitems):
-  #  print(noofitems)
-    if noofitems==0 or size==0:
-        return 0
-    
-    
-    if (noofitems,size) in ans:
-        return ans[(noofitems,size)]
-    
-    if size < weights[noofitems-1]:
-        t=knapsack(ans,size,noofitems-1)
-    else:
-        t=max(knapsack(ans,size,noofitems-1),
-              knapsack(ans, size-weights[noofitems-1], noofitems-1)+ values[noofitems-1] )
-    ans[(noofitems,size)]=t
-    return t
-
 import sys
-sys.setrecursionlimit(10000)
-#print(ans)
-print(knapsack(ans,capacity,noofitems))
+counterReference = 0
+def main():
+    file = open("BigKnapsack.txt","r")
+    knapsackInfo = file.readline().split(" ")
+    noOfItems = int(knapsackInfo[1])
+    capacity = int(knapsackInfo[0])
+    counterReference = noOfItems
+    weights = []
+    values = []
+    for i in range(noOfItems):
+        tempItemInfo = file.readline().split(" ")
+        weights.append(int(tempItemInfo[1]))
+        print(i)
+        values.append(int(tempItemInfo[0]))
+    ans = {}
+
+    sys.setrecursionlimit(10000)
+    print(knapsackBig(ans, capacity, noOfItems, weights, values))
+
+def knapsackBig(ans, size, noOfItems, weights, values):
+    print("IN PROCESS...Please wait for counter to reach somewhere near 2000   : " + str(noOfItems)  )
+    if noOfItems == 0 or size == 0:
+        return 0
+    if (noOfItems, size) in ans:
+        return ans[(noOfItems, size)]
+
+    if size < weights[noOfItems - 1]:
+        optimalAnswer = knapsackBig(ans, size, noOfItems - 1, weights, values)
+    else:
+        optimalAnswer = max(knapsackBig(ans, size, noOfItems - 1, weights, values),
+              knapsackBig(ans, size - weights[noOfItems - 1], noOfItems - 1, weights, values) + values[noOfItems - 1] )
+
+    ans[(noOfItems, size)] = optimalAnswer
+    return optimalAnswer
+
+if __name__ == "__main__":
+    main()
