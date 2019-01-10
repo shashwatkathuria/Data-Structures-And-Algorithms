1541dc7c770c21a1592a2ac85f20452ea0027c73
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-01-10 20:49:30 +0530

    New Class Heap created from the original program and appropriate variables added, restructured code.Only comments to be added.

--- Median Maintenance - Using Heaps/MedianMaintenance.py
+++ Median Maintenance - Using Heaps/MedianMaintenance.py
@@ -2,184 +2,162 @@
 """
 Created on Thu Jul  5 18:39:35 2018
 
-@author: shash
+@author: Shashwat Kathuria
 """
-import time
-start=time.time()
-heap=['Nan']*10000
-i1=1
-i2=1
-def extractmin(heap):
-    return heap[1]
-  
-def insert1(heap,element,i):
-    global i1
-    heap[i]=element
-    parenti=i//2
-    try:
-     if parenti!=0 and heap[i]<heap[parenti]:
-       heap[i],heap[parenti]=heap[parenti],heap[i]
-       insert1(heap,element,parenti)
-     else:
-        i1+=1
-        return
-    except:
-        heap[i]='Nan'
-        insert1(heap,element,parenti)
-        return
-def insert2(heap,element,i):
-    global i2
-    heap[i]=element
-    parenti=i//2
-    try:
-      if parenti!=0 and heap[i]<heap[parenti]:
-        heap[i],heap[parenti]=heap[parenti],heap[i]
-        insert2(heap,element,parenti)
-      else:
-        i2+=1
-        return
-    except:
-        heap[i]='Nan'
-        insert2(heap,element,parenti)
-        return
-def removemin(heap,i):
-    if heap[i]=='Nan':
-        return
-    child1=2*i
-    child2=(2*i)+1
-    if heap[child1]=='Nan' and heap[child2]=='Nan':
-        heap[i]='Nan'
-        return
-    elif heap[child2]=='Nan':
-        if heap[child1]>heap[i]:
-            heap[i],heap[child1]=heap[child1],heap[i]
-            heap[child1]='Nan'
+def main():
+
+    file = open("Median.txt", "r")
+
+    noOfElements = int( file.readline() )
+    listOfElements = []
+
+    for i in range(noOfElements):
+        element = int(file.readline()[:-1])
+        listOfElements.append(element)
+
+    print( noOfElements )
+    print( listOfElements )
+
+    minHeap = Heap( noOfElements, 30 )
+    maxHeap = Heap( noOfElements, 30 )
+
+    n1 = minHeap.getI()
+    n2 = maxHeap.getI()
+
+    medians = []
+    medians.append( listOfElements[0] )
+    medians.append( listOfElements[1] )
+
+    minHeap.insertElement(-listOfElements[1], minHeap.getI())
+    maxHeap.insertElement( listOfElements[0], maxHeap.getI())
+
+    for i in range(2, noOfElements):
+        print("CALCULATING.. CURRENTLY ON ITERATION NO "+ str(i) + " OUT OF " + str(noOfElements) )
+        if n1 == n2:
+            if listOfElements[i] < maxHeap.extractMinimum():
+                minHeap.insertElement(-listOfElements[i], minHeap.getI())
+                medians.append(-minHeap.extractMinimum())
+                n1 += 1
+            else:
+                maxHeap.insertElement(listOfElements[i], maxHeap.getI())
+                medians.append(maxHeap.extractMinimum())
+                n2 += 1
+
+        elif n1 == n2 + 1:
+            if listOfElements[i] > ( minHeap.extractMinimum() * ( - 1 ) ):
+                maxHeap.insertElement(listOfElements[i], maxHeap.getI())
+                medians.append(-minHeap.extractMinimum())
+                n2 += 1
+            else:
+                exchangeElementHeap = minHeap.extractMinimum() * ( - 1 )
+                minHeap.removeMinimum()
+                maxHeap.insertElement(exchangeElementHeap, maxHeap.getI())
+                minHeap.insertElement(-listOfElements[i],minHeap.getI())
+                medians.append(-minHeap.extractMinimum())
+                n2 += 1
+
+        elif n2 == n1 + 1:
+            if listOfElements[i] < maxHeap.extractMinimum():
+                minHeap.insertElement(-listOfElements[i], minHeap.getI())
+                medians.append(-minHeap.extractMinimum())
+                n1 += 1
+            else:
+                exchangeElementHeap = maxHeap.extractMinimum()
+                maxHeap.removeMinimum()
+                minHeap.insertElement(-exchangeElementHeap, minHeap.getI())
+                maxHeap.insertElement(listOfElements[i], maxHeap.getI())
+                medians.append(-minHeap.extractMinimum())
+                n1 += 1
+
+
+    print("\n\nThe sum of the medians over all the iterations is : " + str( sum(medians) ) + "\n\n")
+
+class Heap:
+
+    def __init__(self, noOfElements, limitOfRestructuring):
+        self.heap = [ 'NaN' ] * noOfElements
+        self.noOfElements = noOfElements
+        self.i = 1
+        self.noOfRemovedElements = 0
+        self.limitOfRestructuring = limitOfRestructuring
+
+    def __str__(self):
+        string = "["
+        for i in range(1, self.i , 1):
+            string += str(self.heap[i]) + " "
+        return string + "]"
+
+    def extractMinimum(self):
+        return self.heap[1]
+
+    def getI(self):
+        return self.i
+
+    def insertElement(self, element , i ):
+        self.heap[i] = element
+        parenti = i // 2
+        try:
+
+            if parenti != 0 and self.heap[i] < self.heap[parenti]:
+                self.heap[i], self.heap[parenti] = self.heap[parenti], self.heap[i]
+                self.insertElement(element, parenti)
+            else:
+                self.i += 1
+                return
+
+        except:
+            self.heap[i] = 'NaN'
+            self.insertElement(element, parenti)
             return
-    elif heap[child1]=='Nan':
-        if heap[child2]>heap[i]:
-            heap[i],heap[child2]=heap[child2],heap[i]
-            heap[child2]='Nan'
+
+    def removeMinimum(self, i = 1):
+
+        if self.noOfRemovedElements == self.limitOfRestructuring:
+            self.restructureHeap()
+            self.noOfRemovedElements  = 0
+
+        if self.heap[i] == 'NaN' :
+            self.noOfRemovedElements += 1
             return
-        
+        if 2 * i + 1 > self.noOfElements or 2 * i > self.noOfElements:
+            self.heap[i] == "NaN"
+            self.noOfRemovedElements += 1
+            return
 
-    if heap[child1]<=heap[child2]:
-            heap[i],heap[child1]=heap[child1],heap[i]
-            removemin(heap,child1)
-    else:
-            heap[i],heap[child2]=heap[child2],heap[i]
-            removemin(heap,child2)
+        child1 = 2 * i
+        child2 = ( 2 * i ) + 1
 
-def check1(heap):
-      global i1
-      i1=1  
-      decker=[]
-      for h in heap:
-          if h!="Nan":
-              decker.append(h)
-      heap=["Nan"]*100000
-      for deck in decker:
-          insert1(heap,deck,i1)
-      return heap
-          
-def check2(heap):
-      global i2
-      i2=1
-      decker=[]
-      for h in heap:
-          if h!="Nan":
-              decker.append(h)        
-      heap=["Nan"]*100000
-      for deck in decker:
-          insert2(heap,deck,i2)
-      return heap
+        if self.heap[child1] == 'NaN' and self.heap[child2] == 'NaN':
+            self.heap[i] = 'NaN'
+            self.noOfRemovedElements += 1
+            return
 
+        elif self.heap[child2] == 'NaN':
+            self.heap[i], self.heap[child1] = self.heap[child1], "NaN"
+            self.noOfRemovedElements += 1
+            return
 
+        elif self.heap[child1] == 'NaN':
+            self.heap[i], self.heap[child2] = self.heap[child2], "NaN"
+            self.noOfRemovedElements += 1
+            return
 
-l=[]
-file=open("Median.txt","r")
-for i in range(10000):
-    s=file.readline()
-    s=int(s[:len(s)-1])
-    l.append(s)
-
-
-n1=i1
-n2=i2
-medians=[]
-heaplow=['Nan']*100000
-insert1(heaplow,(-l[1]),i)
-heaphigh=['Nan']*100000
-insert2(heaphigh,l[0],i2)
-medians.append(6331)
-medians.append(2793)
-for i in range(2,10000):
-
-#    print(heaplow[1:i1])
-#    print(heaphigh[1:i2])
-#    print("The medians{}".format(medians))
-    if n1==n2:
-         if l[i]<heaphigh[1]:
-             insert1(heaplow,-l[i],i1)
-             medians.append(-heaplow[1])
-             n1+=1
-         else:
-             insert2(heaphigh,l[i],i2)
-             medians.append(heaphigh[1])
-             n2+=1
-    elif n1==n2+1:
-         if l[i]>(heaplow[1]*(-1)):
-             insert2(heaphigh,l[i],i2)
-             medians.append(-heaplow[1])
-             n2+=1
-             
-         else:
-             gadbadh=heaplow[1]*(-1)
-             removemin(heaplow,1)
-             insert2(heaphigh,gadbadh,i2)
-             insert1(heaplow,-l[i],i1)
-             medians.append(-heaplow[1])
-             n2+=1
-             
-    elif n2==n1+1:
-        if l[i]<heaphigh[1]:
-            insert1(heaplow,-l[i],i1)
-            medians.append(-heaplow[1])
-            n1+=1
+        if self.heap[child1] <= self.heap[child2]:
+            self.heap[i], self.heap[child1] = self.heap[child1], self.heap[i]
+            self.removeMinimum( child1 )
         else:
-            gadbadh=heaphigh[1]
-            removemin(heaphigh,1)
-            insert1(heaplow,-gadbadh,i1)
-            insert2(heaphigh,l[i],i2)
-            medians.append(-heaplow[1])
-            n1+=1
-    if i%3000==0:
-     heaplow=check1(heaplow) 
-     heaphigh=check2(heaphigh)
-#print()
-#print()        
-#print(heaplow[1:i1])
-#print(heaphigh[1:i2])
-end=time.time()
-print()
-print()
-#print(medians)
-print(sum(medians))
-print(end-start)
-            
-    
+            self.heap[i], self.heap[child2] = self.heap[child2], self.heap[i]
+            self.removeMinimum( child2 )
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    def restructureHeap(self):
+        self.i = 1
+        tempList = []
+        for heapElement in self.heap:
+            if heapElement != "NaN" :
+                tempList.append( heapElement )
+        self.heap = ["NaN"] * self.noOfElements
+        for element in tempList:
+            self.insertElement(element, self.i)
+
+if __name__ == "__main__":
+    main()
