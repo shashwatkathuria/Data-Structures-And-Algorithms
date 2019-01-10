c3815044ae3d4b21048445b31c72ee5b03cb9a5c
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-01-11 01:16:33 +0530

    Added all the changes to code

--- Hash Table Implementation - 2 Sum Algorithm/2SUM.py
+++ Hash Table Implementation - 2 Sum Algorithm/2SUM.py
@@ -2,49 +2,73 @@
 """
 Created on Fri Jul  6 14:30:13 2018
 
-@author: shash
+@author: Shashwat Kathuria
 """
-l1=[[0]]*1328263
-def hashfunction(number):
-    if number<0:
-        number=abs(number)
-        number+=17
-    str1=str(number)
-    rem=number%17
-    ans=""
-    for i in range(len(str1)):
-        if i%2==0:
-            ans+=str1[i]
-    ans+=str(rem)
-    for i in range(len(str1)):
-        if i%2==1:
-            ans+=str1[i]
-    return int(ans)%1328263
-file=open("2SUM.txt","r")
-l=[]
-for i in range(1000000):
-    z=int(file.readline())
-    l.append([z,hashfunction(z)])
-#assert False
-file=open("2SUM.txt","r")
+hashTable = [ [0] ] * 111827
 
-for i in range(1000000):
-      print(i)
-#      print(hashfunction(s))
-#      print(s)
-      ltemp=[]
-   #   if s not in l1[hashfunction(s)]:
-      ltemp=(l1[l[i][1]])[:]#.append(s)
-      ltemp.append(l[i][0])
-      l1[l[i][1]]=ltemp[:]
-    #  print(l1[hashfunction(s)])
+def main():
 
-foundvalues=[]
-for s in range(-9980,-9965,1):
-      print(s)
-      for pair in l:
-          n=s-pair[0]
-          if n in l1[hashfunction(n)]:
-              foundvalues.append(s)
-              break
-        
+    file = open("2SUM.txt", "r")
+
+    listOfElements = []
+    for i in range(1000000):
+        element = int( file.readline() )
+        listOfElements.append([element, hashFunction(element)])
+        print("READING ELEMENT NUMBER " + str(i + 1))
+
+
+    for i in range(1000000):
+        print("INSERTING ELEMENT NUMBER " + str(i + 1))
+
+        temp = []
+        temp = ( hashTable[listOfElements[i][1]] ) [ : ]
+
+        if listOfElements[i][0] not in temp:
+            temp.append(listOfElements[i][0])
+            hashTable[listOfElements[i][1]] = temp[:]
+
+    print("\n\nANALYZING THE DISTRIBUTION OF ELEMENTS IN HASH TABLE:-\n\n")
+
+    ctr = 0
+    for i in hashTable:
+        ctr+=1
+        print("Length of the list number " + str(ctr) + " in hash table is " + str( len(i) ) )
+
+
+    i = 0
+    answer = []
+    for sum in range(-10000, 10000+1):
+        print("COMPUTING ON SUM : " + str(sum))
+        for element in  listOfElements:
+            otherElement = sum - element[0]
+            hashCalc = hashFunction(otherElement)
+            flag = False
+            for hashElement in hashTable[ hashCalc ]:
+                if hashElement == otherElement:
+                    i += 1
+                    answer.append(sum)
+                    print("=> " + str(sum) + " SATISFIES THE 2 Sum PROPERTY")
+                    print("=> Total number of sums satisfying the 2 Sum property till now is : " + str(i))
+                    flag = True
+                    break
+            if flag == True:
+                break
+
+    print("\nThe following sums satisfy the 2 Sum property : \n" + str(answer))
+    print("\nThe total number of sums satisfying 2 SUM property is : " + str(i) + "\n\n")
+
+def hashFunction(number):
+    if number < 0:
+        number = abs( number )
+        number += 17
+    str1 = str( number )
+    remainder = number % 17
+    ans = ""
+    for i in range( len(str1) ):
+        if i % 2 == 0:
+            ans += str1[i]
+    ans += str( remainder )
+    return int(ans) % 111827
+
+if __name__ == "__main__":
+    main()
