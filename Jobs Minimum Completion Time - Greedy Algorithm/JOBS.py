d486802563f467ef76bb29c3998d913d4b895764
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-01-11 04:45:16 +0530

    Changed function body and added Job class.Only comments to be added.

--- Jobs Minimum Completion Time - Greedy Algorithm/JOBS.py
+++ Jobs Minimum Completion Time - Greedy Algorithm/JOBS.py
@@ -2,33 +2,55 @@
 """
 Created on Fri Jul  6 19:16:09 2018
 
-@author: shash
+@author: Shashwat Kathuria
 """
 
-file=open("JOBS.txt","r")
-size=int(file.readline())
-jobs=[]
-for i in range(size):
-    t=(file.readline()).split(" ")
-    t=(int(t[0]),int(t[1]))
-    if int(t[0])==100:
-       print(t)
-    jobs.append(t)
-jobs.sort()
-jobs.reverse()
-mainjobs=[]
+class Job:
 
-#for job in jobs:
-#    mainjobs.append((job[0]-job[1],job[1],job[0]))
-    
-for job in jobs:
-    mainjobs.append((job[0]/job[1],job[1],job[0]))
-    
-mainjobs.sort()
+    def __init__(self, weight, length):
+        self.weight = weight
+        self.length = length
+        self.weightToLengthRatio = float(weight) / float(length)
 
-mainjobs.reverse()
-t=0
-sum=0
-for job in mainjobs:
-    t=(t+job[1])
-    sum+=t*job[2]
+    def __str__(self):
+        return "Weight : " + str(self.weight) + " Length : " + str(self.length) + " Ratio : " + str( self.weightToLengthRatio )
+
+def main():
+
+    file = open("JOBS.txt", "r")
+    noOfJobs = int(file.readline())
+    jobs = []
+    for i in range(noOfJobs):
+        jobInfo = file.readline().split(" ")
+        job = Job( weight = int( jobInfo[0] ), length = int( jobInfo[1] ) )
+        jobs.append(job)
+
+    # for job in jobs:
+    #     print(job)
+
+    # Algorithm 1 - Job weight - Job length
+    jobs.sort( reverse = True, key = lambda x : x.weight )
+    jobs.sort( reverse = True, key = lambda x : x.weight - x.length )
+
+    time = 0
+    wrongCompletionWeight = 0
+    for job in jobs:
+        time += job.length
+        wrongCompletionWeight += time * job.weight
+
+    print("The answer obtained from the WRONG job scheduling greedy algorithm (Weight - Length) is : " + str(wrongCompletionWeight))
+
+
+    # Algorithm 2 - Job weight/ Job length
+    jobs.sort( reverse = True, key = lambda x : x.weightToLengthRatio )
+
+    time = 0
+    correctCompletionWeight = 0
+    for job in jobs:
+        time += job.length
+        correctCompletionWeight += time * job.weight
+
+    print("The answer obtained from the CORRECT job scheduling greedy algorithm (Weight / Length) is : " + str(correctCompletionWeight))
+
+if __name__ == "__main__":
+    main()
