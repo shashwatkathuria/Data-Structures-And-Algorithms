e32ac43ec0b5dcf5b53b2b65ac36cc1fd804bdf3
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-01-11 20:17:06 +0530

    Changed some variable names and indented code some more.Only comments to be added.

--- Sorting - QuickSort/QuickSort.c
+++ Sorting - QuickSort/QuickSort.c
@@ -1,79 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
-unsigned long int ans=0;
+
+
 void quickSort(int arr[], int low, int high);
-int main(){
-	
-	FILE* inptr=fopen("QuickSort.txt","r");
+unsigned long int noOfSwaps = 0;
+
+int main()
+{
+
+	FILE* inptr = fopen("QuickSort.txt", "r");
 	char line[100];
 	int arr[10000];
-	int i=0;
-	for(i=0;i<10000;i++){
-	    fgets(line,sizeof(line),inptr);
-		arr[i]=atoi(line);
+	int i = 0;
+	for(i = 0; i < 10000; i++)
+	{
+	    fgets(line, sizeof(line), inptr);
+			arr[i] = atoi(line);
 	}
 	fclose(inptr);
-	
-	for(i=0;i<10000;i++)
+
+	printf("\nThe original array looks as follows : \n\n");
+	for(i = 0; i < 10000; i++)
 	{
-		printf("%d  \n",arr[i]);
+		printf("%d  \t", arr[i]);
 	}
-	quickSort(arr,0,9999);
-	for(i=0;i<10000;i++)
+	quickSort(arr, 0, 9999);
+
+	printf("\n\nThe sorted array looks as follows : \n\n");
+	for(i = 0; i < 10000; i++)
 	{
-		printf("%d  \n",arr[i]);
+		printf("%d  \t", arr[i]);
 	}
-	printf("     %ld \n",ans);
+
+	printf("\n\nThe number of swaps is :  %ld \n\n", noOfSwaps);
+
 }
-void quickSort(int arr[],int low,int high)
-{   
-    if(low>=high)
-     return;
+
+
+void quickSort(int arr[], int low, int high)
+{
+		if(low >= high)
+		{
+				return;
+		}
+
     int mid;
     int temp;
-    int n=high-low+1;
-    if ((n/2)*2==n)
-        mid=n/2-1;
-    else
-        mid=n/2;
-    int pivot;
-    int z;
-    if((arr[mid]<arr[low] && arr[high]>arr[low])||(arr[mid]>arr[low] && arr[high]<arr[low]))
-    {
-         pivot=arr[low];
-         z=low;
-    }
-    else if ((arr[low]<arr[mid] && arr[high]>arr[mid])||(arr[low]>arr[mid] && arr[high]<arr[mid]))
-         {
-		 pivot=arr[mid];
-		 z=mid;
-    }
-	else
-	{
-         pivot=arr[high];
-         z=high;
-    }
-    temp=arr[low];
-    arr[low]=pivot;
-    arr[z]=temp;
-	int i=low+1,j=low+1;
-	for(j=low+1;j<=high;j++)
-	{
-		if(pivot>arr[j]){
-			temp=arr[i];
-			arr[i]=arr[j];;
-			arr[j]=temp;
-			i++;
+    int noOfElements = high - low + 1;
+    if ( ( noOfElements / 2 ) * 2 == noOfElements )
+		{
+        mid = noOfElements / 2 - 1;
 		}
-		
-	}
-	temp=arr[i-1];
-	arr[i-1]=pivot;
-	arr[low]=temp;
-	
-	ans+=high-low;
-	quickSort(arr,low,i-2);
-	quickSort(arr,i,high);
-	
-	
+		else
+		{
+        mid = noOfElements / 2;
+		}
+
+		int pivot;
+    int pivotPosition;
+		// Computing privot as the median of the start, middle and end elements of the array/subarray in concern
+    if( (arr[mid] < arr[low] && arr[high] > arr[low]) || (arr[mid] > arr[low] && arr[high] < arr[low]) )
+    {
+        pivot = arr[low];
+        pivotPosition = low;
+    }
+    else if ( (arr[low] < arr[mid] && arr[high] > arr[mid]) || (arr[low] > arr[mid] && arr[high] < arr[mid]) )
+    {
+				pivot = arr[mid];
+				pivotPosition = mid;
+    }
+		else
+		{
+         pivot = arr[high];
+         pivotPosition = high;
+    }
+    temp = arr[low];
+    arr[low] = pivot;
+    arr[pivotPosition] = temp;
+
+		int i = low + 1, j = low + 1;
+		for(j = low + 1; j <= high; j++)
+		{
+				if(pivot > arr[j])
+				{
+						temp = arr[i];
+						arr[i] = arr[j];;
+						arr[j] = temp;
+						i++;
+				}
+
+		}
+		temp = arr[i - 1];
+		arr[i - 1] = pivot;
+		arr[low] = temp;
+
+		noOfSwaps += high - low;
+		quickSort(arr, low, i - 2);
+		quickSort(arr, i, high);
+
 }
