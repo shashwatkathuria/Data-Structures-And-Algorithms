2468c757c750de4d0958883573ad12121dfcdff3
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-01-11 21:16:36 +0530

    All changes made.Only comments remaining.

--- Travelling Salesman Problem - Greedy Heuristic/tspgreedy.py
+++ Travelling Salesman Problem - Greedy Heuristic/tspgreedy.py
@@ -2,53 +2,61 @@
 """
 Created on Tue Jul 10 19:25:25 2018
 
-@author: shash
+@author: Shashwat Kathuria
 """
 
-file=open("tspheuristic.txt","r")
-s=int(file.readline())
-noofcities=s
-cities={}
-for i in range(noofcities):
-    s=file.readline().split()
-    s=[float(s[1]),float(s[2])]
-    cities[i+1]=s[:]
-ordering=list(range(2,noofcities+1))
+# The answer is 1203406.5012708856
 
-def distbwcities(citya,cityb):
-    return (((citya[0]-cityb[0])**2)+((citya[1]-cityb[1])**2))**0.5
+visitedCities = []
+notVisitedCities = []
+noOfCities = 0
+cities = {}
+distance = []
 
+def main():
 
+    global visitedCities, notVisitedCities, cities, noOfCities
 
-def distfromcityi(i,visited,notvisited):
-    dist=[]
-    for j in notvisited:
-           dist.append([distbwcities(cities[i],cities[j]),j])
-    dist.sort()
-    return dist[0]
+    file = open("TSP.txt", "r")
+    noOfCities = int( file.readline() )
 
-visited=[1]
-notvisited=list(range(2,noofcities+1))
-def tspheuristic():
-    s1=0
-    for k in range(1,noofcities):
-        
-        vertex=visited[-1]
-        [distanceneeded,destination]=distfromcityi(vertex,visited,notvisited)
-        s1+=distanceneeded
-        visited.append(destination)
-        notvisited.remove(destination)
-        print(destination)
-    print(s1+distbwcities(cities[1],cities[visited[-1]]))
-#ans=1203406.5012708856
-#def tspheuristic():
-#    s1=0
-#    i=1
-#    while len(visited)!=noofcities:
-#       print(len(visited))
-#       s1+=distbwcities(cities[i],cities[i+1]) 
-#       visited.append(i)
-#    print(s1+distbwcities(cities[33708],cities[1]))
-tspheuristic()
-    
+    for i in range(noOfCities):
+        cityCoordinates = file.readline().split()
+        cities[i + 1] = [float(cityCoordinates[1]), float(cityCoordinates[2])]
 
+    visitedCities = [1]
+    notVisitedCities = list( range(2, noOfCities + 1) )
+    tspGreedyHeuristic()
+
+def tspGreedyHeuristic():
+
+    global visitedCities, notVisitedCities, cities, noOfCities
+
+    distanceCalc = 0
+    for k in range(1, noOfCities):
+        print( "COMPUTING...ON ITERATION NUMBER : " + str(k) + " OUT OF " + str(noOfCities - 1) )
+        distanceToBeAdded, nextCity = distanceFromCityi(lastVisitedVertex = visitedCities[-1],notVisitedCities = notVisitedCities)
+        distanceCalc += distanceToBeAdded
+        visitedCities.append(nextCity)
+        notVisitedCities.remove(nextCity)
+
+    print("The start vertex is : " + str(visitedCities[0]))
+    print("The end vertex is : " + str(visitedCities[-1]))
+    print(distanceCalc + distanceBetweenCities(cities[1], cities[visitedCities[-1]]) )
+
+def distanceBetweenCities(CityA, CityB):
+
+    return ( ( (CityA[0] - CityB[0]) ** 2) + ( (CityA[1] - CityB[1]) ** 2) )**0.5
+
+def distanceFromCityi(lastVisitedVertex, notVisitedCities):
+
+    distance = []
+
+    for j in notVisitedCities:
+           distance.append( [distanceBetweenCities(cities[lastVisitedVertex], cities[j]), j] )
+    distance.sort(reverse = False, key = lambda x: x[0])
+
+    return distance[0][0],distance[0][1]
+
+if __name__ == "__main__":
+    main()
