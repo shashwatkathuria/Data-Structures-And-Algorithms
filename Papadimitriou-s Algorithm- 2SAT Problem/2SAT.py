293482b9c781a703a68f8761884e428e668553ce
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-01-11 05:54:16 +0530

    Created Main.Only comments to be added.

--- Papadimitriou-s Algorithm- 2SAT Problem/2SAT.py
+++ Papadimitriou-s Algorithm- 2SAT Problem/2SAT.py
@@ -31,59 +31,62 @@ class Clause:
 
            return boolean1 or boolean2
 
+def main():
+    file = open("2SAT1.txt", "r")
+    noOfClauses = int( file.readline() )
 
-file = open("2SAT1.txt", "r")
-noOfClauses = int( file.readline() )
-
-clauses = []
-for i in range(noOfClauses):
-    clauseLiteralsInfo = file.readline().split()
-    clause = Clause( firstLiteral = int(clauseLiteralsInfo[0]), secondLiteral = int(clauseLiteralsInfo[1]))
-    clauses.append(clause)
-
-
-
-answers = []
-answers.append( "NaN" )
-for x in range(1, noOfClauses + 1):
-    x = random.random()
-    if x > 0.5:
-        answers.append(True)
-    else:
-        answers.append(False)
+    clauses = []
+    for i in range(noOfClauses):
+        clauseLiteralsInfo = file.readline().split()
+        clause = Clause( firstLiteral = int(clauseLiteralsInfo[0]), secondLiteral = int(clauseLiteralsInfo[1]))
+        clauses.append(clause)
 
 
 
-twoNSquared = 2 * (noOfClauses ** 2)
-for k in range( int( math.log(noOfClauses, 2) ) ):
+    answers = []
+    answers.append( "NaN" )
+    for x in range(1, noOfClauses + 1):
+        x = random.random()
+        if x > 0.5:
+            answers.append(True)
+        else:
+            answers.append(False)
 
-    print(k)
 
-    for j in range(twoNSquared):
 
-        flag = True
-        noOfSatisfyingClauses = 0
-        for clause in clauses:
-            randomlyChosenLiteral = random.choices([clause.firstLiteral,clause.secondLiteral])[0]
-            if clause.isSatisfyingCriteria() == False:
-                flag = False
-                answers[ abs(randomlyChosenLiteral) ] = not( answers[ abs(randomlyChosenLiteral) ] )
+    twoNSquared = 2 * (noOfClauses ** 2)
+    for k in range( int( math.log(noOfClauses, 2) ) ):
+
+        print(k)
+
+        for j in range(twoNSquared):
+
+            flag = True
+            noOfSatisfyingClauses = 0
+            for clause in clauses:
+                randomlyChosenLiteral = random.choices([clause.firstLiteral,clause.secondLiteral])[0]
+                if clause.isSatisfyingCriteria() == False:
+                    flag = False
+                    answers[ abs(randomlyChosenLiteral) ] = not( answers[ abs(randomlyChosenLiteral) ] )
+                    break
+                else:
+                    noOfSatisfyingClauses += 1
+
+            print("The number of clauses satisfying criteria till now (of the ones which have been checked) is :" + str(noOfSatisfyingClauses) )
+
+            if flag == True:
+                isSatisfying = True
                 break
-            else:
-                noOfSatisfyingClauses += 1
 
-        print("The number of clauses satisfying criteria till now (of the ones which have been checked) is :" + str(noOfSatisfyingClauses) )
-
-        if flag == True:
-            isSatisfying = True
+        if isSatisfying == True:
+            print("The answers of the literals respectively are as follows :- \n\n")
+            for i in range(1, len(answers),1):
+                print("Literal " + str(i) + " : " + str(answers[i]) )
+            print("The instance is satisfiable")
             break
 
-    if isSatisfying == True:
-        print("The answers of the literals respectively are as follows :- \n\n")
-        for i in range(1, len(answers),1):
-            print("Literal " + str(i) + " : " + str(answers[i]) )
-        print("The instance is satisfiable")
-        break
+    if isSatisfying == False:
+        print("The instance is not satisfiable")
 
-if isSatisfying == False:
-    print("The instance is not satisfiable")
+if __name__ == "__main__":
+    main()
