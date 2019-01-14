8715a6d7a3a65486f3b5a16aff10112f51f6f3a5
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-01-14 19:06:29 +0530

    Added comments and documentation.Everything done.

--- Sorting - MergeSort/MergeSort.c
+++ Sorting - MergeSort/MergeSort.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// MERGE SORT
+
+// Declaring functions required
 void mergeSort(int arr[], int low, int high);
 void merge(int arr[], int low, int mid, int high);
+
+// Declaring a variable to keep track of the number of inversions
 unsigned long long int noOfInversions = 0;
 
 int main()
 {
+    // Reading input elements from the file and storing inside an array
     FILE* inptr = fopen("IntegerArray.txt", "r");
     int arr[100000];
     int x;
@@ -20,38 +26,51 @@ int main()
     }
     fclose(inptr);
 
+    // Printing input array
     printf("\nThe input array is as follows : \n\n");
     for (i = 0; i < 100000; i++)
     {
         printf("%d \t ", arr[i]);
     }
+
+    // Calling merge sort algorithm on array
     mergeSort(arr, 0, 99999);
 
+    // Printing final sorted array
     printf("\n\nThe sorted array is as follows : \n\n");
     for (i = 0; i < 100000; i++)
     {
         printf("%d \t ", arr[i]);
     }
+
+    // Printing the number of inversions encountered in the array
     printf("\nThe number of inversions in this array is :  %lld \n", noOfInversions);
 
 }
 
+
 void mergeSort(int arr[], int low, int high)
 {
+    // Recursion case
 	  if (low < high)
 	  {
+        // calling merge sort on the two halves of the array and then merging them
 	      int mid = (low + high) / 2;
 		    mergeSort(arr, low, mid);
 	      mergeSort(arr, mid + 1, high);
 	      merge(arr, low, mid, high);
     }
+    // Base case
     else
     {
         return;
     }
 }
+
+
 void merge(int arr[],int low,int mid,int high)
 {
+    // Declaring two copies of the halves of the array on which merging is to be done
 	  int arr1[50000];
 	  int arr2[50000];
 	  int n1 = mid - low + 1;
@@ -63,9 +82,11 @@ void merge(int arr[],int low,int mid,int high)
     }
     for(i = 0; i < n2; i++)
     {
-	    arr2[i]=arr[mid+1+i];
+	    arr2[i] = arr[mid + 1 + i];
     }
     i = j = 0;
+
+    // Merging the two halves (copies)
 	  k = low;
     while(i < n1 && j < n2 && k <= high)
     {
@@ -74,6 +95,7 @@ void merge(int arr[],int low,int mid,int high)
     		    arr[k] = arr2[j];
     		    j++;
     		    k++;
+            // Incrementing number of inversions
     		    noOfInversions += (n1 - i);
 		    }
 		    else
@@ -83,6 +105,8 @@ void merge(int arr[],int low,int mid,int high)
 			      k++;
 		    }
 	  }
+
+    // Appending to the result the left out elements of right half
 	  if(i == n1 && j < n2)
 	  {
 		    while(j < n2 && k <= high)
@@ -92,6 +116,8 @@ void merge(int arr[],int low,int mid,int high)
 			      k++;
 		    }
 	  }
+
+    // Appending to the result the left out elements of the left half
 	  else if (i < n1 && j == n2)
 	  {
 		    while(i < n1 && k <= high)
