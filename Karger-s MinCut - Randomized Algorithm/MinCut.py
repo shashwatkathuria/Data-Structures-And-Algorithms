5c3d925d55ed58045333f0478c90c35b575c6174
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-01-19 22:42:40 +0530

    Modified procedure to get the new edges list after merging vertices to make algorithm more efficient.Everything done.

--- Karger-s MinCut - Randomized Algorithm/MinCut.py
+++ Karger-s MinCut - Randomized Algorithm/MinCut.py
@@ -5,15 +5,15 @@ Created on Sat May 26 17:13:57 2018
 @author: Shashwat Kathuria
 """
 
-# KARGER'S RANDOMIZED MINCUT ALGORITHM (FOR UNDIRECTED GRAPH) 
+# KARGER'S RANDOMIZED MINCUT ALGORITHM (FOR UNDIRECTED GRAPH)
 
 import random
 
 # Declaring lists to store edges
-edgesList, edgeListCopy1, edgeListCopy2 = [], [], []
+edgesList, edgeListCopy1, newModifiedEdgeList = [], [], []
 
 def main():
-        global edgeListCopy1, edgeListCopy2, edgesList
+        global edgeListCopy1, newModifiedEdgeList, edgesList
 
         # Reading input file and storing edges is a list
         noOfVertices = 200
@@ -53,38 +53,53 @@ def main():
                 # Merging edge and its vertices
                 saveVertex, removeVertex = getVertexMergingCriteria( randomEdge )
 
-                edgeListCopy2 = edgeListCopy1[:]
+                # Declaring modified new edge list
+                newModifiedEdgeList = []
 
-                # Removing that edge from the edge copy lists
-                edgeListCopy2.remove( randomEdge )
-                edgeListCopy1.remove( randomEdge )
+                for edgeNo in range(len(edgeListCopy1)):
 
-                # Renaming adjacent edges with the name of merged vertex
-                for edge in edgeListCopy1:
+                        # Not adding the random edge to new edge list
+                        if edgeListCopy1[edgeNo] == randomEdge:
+                            continue
 
-                        if removeVertex == edge[0]:
-                            shiftVertexPosition = edge[1]
-                            edgeListCopy2.remove(edge)
+                        # Renaming adjacent edges with the name of merged vertex
+                        elif removeVertex == edgeListCopy1[edgeNo][0]:
+                            shiftVertexPosition = edgeListCopy1[edgeNo][1]
+                            # newModifiedEdgeList.remove(edgeListCopy1[edgeNo])
                             modifiedMergedEdge = ( saveVertex, shiftVertexPosition )
-                            edgeListCopy2.append( modifiedMergedEdge )
 
-                        if removeVertex == edge[1]:
-                            shiftVertexPosition = edge[0]
-                            edgeListCopy2.remove(edge)
+                            # Checking for self loop
+                            if modifiedMergedEdge[0] != modifiedMergedEdge[1]:
+                                newModifiedEdgeList.append( modifiedMergedEdge )
+
+                        # Renaming adjacent edges with the name of merged vertex
+                        elif removeVertex == edgeListCopy1[edgeNo][1]:
+                            shiftVertexPosition = edgeListCopy1[edgeNo][0]
+                            # newModifiedEdgeList.remove(edgeListCopy1[edgeNo])
                             modifiedMergedEdge = ( saveVertex, shiftVertexPosition )
-                            edgeListCopy2.append( modifiedMergedEdge )
 
-                # Deleting self loops
-                deleteSelfLoops()
-                edgeListCopy1 = edgeListCopy2[:]
+                            # Checking for self loop
+                            if modifiedMergedEdge[0] != modifiedMergedEdge[1]:
+                                newModifiedEdgeList.append( modifiedMergedEdge )
+
+                        else:
+                            # Checking for self loop
+                            if edgeListCopy1[edgeNo][0] != edgeListCopy1[edgeNo][1]:
+                                newModifiedEdgeList.append(edgeListCopy1[edgeNo])
+
+                # We update the newModifiedEdgeList to be an empty list in the next
+                # iteration, assuming no aliasing effect of that on edgeListCopy1, this works
+                # more efficiently then if we copy the newModifiedEdgeList to edgeListCopy1
+                edgeListCopy1 = newModifiedEdgeList#[:]
 
             # Adding MinCut of current iteration to possibilities list
-            possibilities.append(len(edgeListCopy2))
+            possibilities.append(len(newModifiedEdgeList))
 
             # Printing result found till now
             print("MinCut obtained till now : " + str(min(possibilities)) + "\t\tLast added MinCut :" + str(possibilities[ - 1 ]) + "\n\n ")
 
 def getVertexMergingCriteria(edge):
+
     # Criteria to merge the 2 vertices of an edge into 1
     # The one with small numerical value represents the merged vertex
     if edge[0] > edge[1]:
@@ -92,16 +107,6 @@ def getVertexMergingCriteria(edge):
     else:
         return edge[0], edge[1]
 
-def deleteSelfLoops():
-    global edgeListCopy1, edgeListCopy2, edgesList
-
-    # temp defined to eliminate aliasing effect
-    temp = edgeListCopy2[:]
-
-    # Deleting self loops
-    for edge in temp:
-        if edge[0] == edge[1]:
-            edgeListCopy2.remove(edge)
 
 if __name__ == "__main__":
      main()
