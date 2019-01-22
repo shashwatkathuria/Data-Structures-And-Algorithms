caa9e55b385cf5e7f7b6aac0a82d340dc734cc70
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-01-22 22:31:48 +0530

    Written program algorithm alongwith backtracking.Comments and some indentation to be added.

--- DNA Sequence Alignment - Using Dynamic Programming/sequencealignment.cpp
+++ DNA Sequence Alignment - Using Dynamic Programming/sequencealignment.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<fstream>
 #include<string.h>
+#include<algorithm>
+#include<iomanip>
 
 using namespace std;
 
@@ -14,6 +16,7 @@ int main(void)
     cout<<"\nEnter second sequence : ";
     cin>>sequence2;
     DNASequenceAlignment(sequence1, sequence2);
+    return 0;
 }
 
 void DNASequenceAlignment(string sequence1, string sequence2)
@@ -48,37 +51,115 @@ void DNASequenceAlignment(string sequence1, string sequence2)
                 }
             }
         }
+
     }
 
-    //Printing the dpMatrix
     cout << endl;
+    cout<<"The Dynamic Programming Matrix is as follows : \n";
     for(int i = 0; i < m ; i++ )
     {
 
-        //Printing rows in required format
         if(i == 0)
         {
-            //Printing string s2
-            cout << endl << "     #";
+            cout << endl <<" "<<setw (10)<< "#";
             for(int k = 0; k < n; k++)
             {
-                cout << "  " << sequence2[k];
+                cout << setw(10) << sequence2[k];
             }
-            cout << endl << "#  ";
+            cout <<endl<< endl << "#";
         }
 
-        //Printing string s1 letter by letter
         if(i != 0)
         {
-            cout << sequence1[i - 1] << "  ";
+            cout <<sequence1[i - 1];
 
           }
-        //Printing columns in required format
         for(int j = 0; j < n ; j++)
         {
-            cout << "  " << dpMatrix[i][j];
+            cout << setw(10) << dpMatrix[i][j];
         }
         cout << endl;
     }
     cout << endl;
+
+
+
+    int i = m - 1,j = n - 1;
+    string seq1,seq2;
+    while(i > 0 && j > 0)
+    {
+        int option1,option2,option3;
+        if(sequence1[i - 1] == sequence2[j - 1])
+        {
+            option1 = dpMatrix[i - 1][j - 1] + 0;
+            option2 = dpMatrix[i - 1][j] + 2;
+            option3 = dpMatrix[i][j - 1] + 2;
+            if (dpMatrix[i][j] == option1)
+            {
+                seq1.push_back(sequence1[i - 1]);
+                seq2.push_back(sequence2[j - 1]);
+                i--;
+                j--;
+
+            }
+            else if (dpMatrix[i][j] == option2)
+            {
+                seq1.push_back(sequence1[i - 1]);
+                seq2.push_back('#');
+                i--;
+            }
+            else if (dpMatrix[i][j] == option3)
+            {
+                seq1.push_back('#');
+                seq2.push_back(sequence2[j - 1]);
+                j--;
+            }
+
+        }
+        else if (sequence1[i - 1] != sequence2[j - 1])
+        {
+            option1 = dpMatrix[i - 1][j - 1] + 1;
+            option2 = dpMatrix[i - 1][j] + 2;
+            option3 = dpMatrix[i][j - 1] + 2;
+            if (dpMatrix[i][j] == option1)
+            {
+                seq1.push_back(sequence1[i - 1]);
+                seq2.push_back(sequence2[j - 1]);
+                i--;
+                j--;
+
+            }
+            else if (dpMatrix[i][j] == option2)
+            {
+                seq1.push_back(sequence1[i - 1]);
+                seq2.push_back('#');
+                i--;
+            }
+            else if (dpMatrix[i][j] == option3)
+            {
+                seq1.push_back('#');
+                seq2.push_back(sequence2[j - 1]);
+                j--;
+            }
+
+        }
+    }
+    while(j > 0)
+    {
+        seq2.push_back(sequence2[j - 1]);
+        seq1.push_back('#');
+        j--;
+    }
+    while(i > 0)
+    {
+        seq1.push_back(sequence1[i - 1]);
+        seq2.push_back('#');
+        i--;
+    }
+
+    reverse(seq1.begin(), seq1.end());
+    reverse(seq2.begin(), seq2.end());
+    cout<<"\n\nThe best alignment of the two sequences is :\n\n";
+    cout<<"\n\nFIRST STRING  :"<<seq1;
+    cout<<"\nSECOND STRING :"<<seq2<<endl;
 }
