5c03997b0cde9a7dae69773713cae59b26a08e9b
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-01-24 18:29:47 +0530

    Added comments and indentation. Everything done.

--- Travelling Salesman Problem - Greedy Heuristic/tspgreedy.py
+++ Travelling Salesman Problem - Greedy Heuristic/tspgreedy.py
@@ -5,7 +5,11 @@ Created on Tue Jul 10 19:25:25 2018
 @author: Shashwat Kathuria
 """
 
+# TRAVELLING SALESMAN PROBLEM - GREEDY HEURISTIC
+
 # The answer is 1203406.5012708856
+
+# Initializing values required
 
 visitedCities = []
 notVisitedCities = []
@@ -17,6 +21,7 @@ def main():
 
     global visitedCities, notVisitedCities, cities, noOfCities
 
+    # Reading input city coordinates from file and storing them inside a dict
     file = open("TSP.txt", "r")
     noOfCities = int( file.readline() )
 
@@ -24,39 +29,67 @@ def main():
         cityCoordinates = file.readline().split()
         cities[i + 1] = [float(cityCoordinates[1]), float(cityCoordinates[2])]
 
+    # Initializing visited cities with start vertex
     visitedCities = [1]
+
+    # Initializing not visited cities with all vertices except start vertex
     notVisitedCities = list( range(2, noOfCities + 1) )
+
+    # Calling tsp greedy heuristic
     tspGreedyHeuristic()
 
 def tspGreedyHeuristic():
-
+    """Function to calculate minimum Hamiltonian path using greedy heuristic."""
     global visitedCities, notVisitedCities, cities, noOfCities
 
+    # Variable to store the answer
     distanceCalc = 0
+
+    # For loop to run until all the cities are visited
     for k in range(1, noOfCities):
+
+        # Printing iteration number
         print( "COMPUTING...ON ITERATION NUMBER : " + str(k) + " OUT OF " + str(noOfCities - 1) )
+
+        # Calling function to return the next smallest distance from the last
+        # visited city and the city to which it has this distance and storing them
         distanceToBeAdded, nextCity = distanceFromCityi(lastVisitedVertex = visitedCities[-1],notVisitedCities = notVisitedCities)
+
+        # Incrementing the answer by the above obtained distance
         distanceCalc += distanceToBeAdded
+
+        # Appending the city to list of visited cities and removing it from list of not visited cities
         visitedCities.append(nextCity)
         notVisitedCities.remove(nextCity)
 
+    # Printing start and end vertex of path
     print("The start vertex is : " + str(visitedCities[0]))
     print("The end vertex is : " + str(visitedCities[-1]))
-    print(distanceCalc + distanceBetweenCities(cities[1], cities[visitedCities[-1]]) )
+    # Printing result by adding to the answer the last distance so as to complete
+    # the Hamiltonian cycle
+    print("The greedy heuristic answer is : " + str(distanceCalc + distanceBetweenCities(cities[1], cities[visitedCities[-1]])) )
 
 def distanceBetweenCities(CityA, CityB):
+    """Function to return the distance between two cities using their coordinates."""
 
     return ( ( (CityA[0] - CityB[0]) ** 2) + ( (CityA[1] - CityB[1]) ** 2) )**0.5
 
 def distanceFromCityi(lastVisitedVertex, notVisitedCities):
+    """Funtion to return the next smallest city distance and the corresponding city
+       from the last visited vertex."""
 
+    # List to store distances from the last vertex
     distance = []
 
     for j in notVisitedCities:
-           distance.append( [distanceBetweenCities(cities[lastVisitedVertex], cities[j]), j] )
-    distance.sort(reverse = False, key = lambda x: x[0])
+        # Appending all cities
+        distance.append( [distanceBetweenCities(cities[lastVisitedVertex], cities[j]), j] )
 
-    return distance[0][0],distance[0][1]
+    # Storing answer
+    minimumDistanceAndCity = min(distance, key = lambda x: x[0])
+
+    # Returning corresponding answer
+    return minimumDistanceAndCity[0], minimumDistanceAndCity[1]
 
 if __name__ == "__main__":
     main()
