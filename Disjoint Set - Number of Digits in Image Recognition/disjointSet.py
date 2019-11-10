ec4e8fec7868b5e09cd4904022a0cd00ebb83e21
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-11-10 16:15:46 +0530

    Added comments, indentation and made some corrections in program. Completed.

--- Disjoint Set - Number of Digits in Image Recognition/disjointSet.py
+++ Disjoint Set - Number of Digits in Image Recognition/disjointSet.py
@@ -4,61 +4,81 @@ Created on Thu Sep  5 15:16:09 2019
 @author: Shashwat Kathuria
 """
 
+# DISJOINT SET - NUMBER OF DIGITS IN IMAGE RECOGNITION ALGORITHM
+
+# Importing required library class
 from PIL import Image
 
-image = Image.open("i3.png")
+# Initializing required variables
+image = Image.open("image2.png")
+# Converting Image to RGB
 image = image.convert("RGB")
 width, height = image.size
 imagePixels = []
 disjointSets = []
 
-
 def main():
 
+    # Scanning through all the pixels in the image
     for i in range(width):
         for j in range(height):
+            # Getting r, g, b values of image at i, j location
             r, g, b = image.getpixel((i, j))
+            # Storing pixel
             tempPixel = ImagePixel(i, j, r, g, b)
+            # Appending image pixel object to image pixel list
             imagePixels.append(tempPixel)
+            # Adding image to disjoint sets only if black
+            # Because black and white images are being analyzed
+            # With digits being black
             if tempPixel.isBlack:
                 disjointSets.append([tempPixel])
 
-    temp = len(disjointSets)
-    print(len(disjointSets))
-
+    # Scanning through all image pixels
     for i in range(len(imagePixels)):
 
+            # Checking for neighbours of current pixel and merging its
+            # disjoint set to neighbouring disjoint set if condition holds true
             pixel = imagePixels[i]
             print("Reducing...Please Wait..." + str(len(disjointSets)))
+            # Right pixel
             if pixel.isRightPixelNeighbour(image):
                 u = findClusterIndex(pixel.x + 1, pixel.y)
                 v = findClusterIndex(pixel.x, pixel.y)
+                # Merging if not already in same cluster
                 if u != v:
                     mergeClusters(disjointSets[u], disjointSets[v])
+            # Left Pixel
             if pixel.isLeftPixelNeighbour(image):
                 u = findClusterIndex(pixel.x - 1, pixel.y)
                 v = findClusterIndex(pixel.x, pixel.y)
+                # Merging if not already in same cluster
                 if u != v:
                     mergeClusters(disjointSets[u], disjointSets[v])
+            # Upper Pixel
             if pixel.isUpperPixelNeighbour(image):
                 u = findClusterIndex(pixel.x, pixel.y - 1)
                 v = findClusterIndex(pixel.x, pixel.y)
+                # Merging if not already in same cluster
                 if u != v:
                     mergeClusters(disjointSets[u], disjointSets[v])
+            # Bottom Pixel
             if pixel.isBottomPixelNeighbour(image):
                 u = findClusterIndex(pixel.x, pixel.y + 1)
                 v = findClusterIndex(pixel.x, pixel.y)
+                # Merging if not already in same cluster
                 if u != v:
                     mergeClusters(disjointSets[u], disjointSets[v])
 
 
-
+    # Printing answer
     print("The total number of digits in this image is : " + str(len(disjointSets)))
 
-
+# Class for image pixel
 class ImagePixel:
 
     def __init__(self, x, y, r, g, b):
+        """Function to initialize image pixel."""
         self.x = x
         self.y = y
         self.r = r
@@ -68,6 +88,8 @@ class ImagePixel:
         self.set = -1
 
     def isLeftPixelNeighbour(self, image):
+        """Function to check if left image pixel is a neighbour, i.e.,
+        if it is same in colour(black)."""
         if self.x != 0:
             r, g, b = image.getpixel((self.x - 1, self.y))
             isBlack = r == 0 and g == 0 and b == 0
@@ -79,6 +101,8 @@ class ImagePixel:
             return False
 
     def isUpperPixelNeighbour(self, image):
+        """Function to check if upper image pixel is a neighbour, i.e.,
+        if it is same in colour(black)."""
         if self.y != 0:
             r, g, b = image.getpixel((self.x, self.y - 1))
             isBlack = r == 0 and g == 0 and b == 0
@@ -90,6 +114,8 @@ class ImagePixel:
             return False
 
     def isRightPixelNeighbour(self, image):
+        """Function to check if right image pixel is a neighbour, i.e.,
+        if it is same in colour(black)."""
         if self.x != width - 1:
             r, g, b = image.getpixel((self.x + 1, self.y))
             isBlack = r == 0 and g == 0 and b == 0
@@ -101,6 +127,8 @@ class ImagePixel:
             return False
 
     def isBottomPixelNeighbour(self, image):
+        """Function to check if bottom image pixel is a neighbour, i.e.,
+        if it is same in colour(black)."""
         if self.y != height - 1:
             r, g, b = image.getpixel((self.x, self.y + 1))
             isBlack = r == 0 and g == 0 and b == 0
@@ -111,32 +139,32 @@ class ImagePixel:
         else:
             return False
 
-    def find(self, array, x, y):
-        for i in range(len(array)):
-            pixel = array[i]
-            if pixel.x == x and pixel.y == y:
-                return pixel
-
     def __str__(self):
+        """Function to print image pixel."""
         return ("X : " + str(self.x) + " Y : " + str(self.y) + " R : " + str(self.r) + " G : " + str(self.g) + " B : " + str(self.b))
 
 def mergeClusters(cluster1, cluster2):
-
+    """Function to merge two clusters."""
     listi = cluster1
     listj = cluster2
 
+    # Merging and copying to avoid aliasing effect
     ltemp = listi[:] + listj[:]
+
+    # Removing clusters
     disjointSets.remove(listi)
     disjointSets.remove(listj)
+    # Adding merged cluster
     disjointSets.append(ltemp)
 
 def findClusterIndex(x, y):
+    """Function to find cluster index."""
+
     for i in range( len(disjointSets) ):
         for element in disjointSets[i]:
             if element.x == x and element.y == y and element.isBlack:
                 return i
 
-
-
+# Calling main function
 if __name__ == "__main__":
     main()
